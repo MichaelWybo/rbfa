4af9ec62239b212542b2cb56a1d38c08b06b28db
MichaelWybo <32988994+MichaelWybo@users.noreply.github.com>
2025-12-14 14:55:57 +0100

    Add device info to Elgato entity initialization

--- custom_components/rbfa/entity.py
+++ custom_components/rbfa/entity.py
@@ -12,3 +12,9 @@ class RbfaEntity(CoordinatorEntity[MyCoordinator]):
     def __init__(self, coordinator: MyCoordinator) -> None:
         """Initialize an Elgato entity."""
         super().__init__(coordinator=coordinator)
+        self._attr_device_info = {
+            "identifiers": {("rbfa", coordinator.config_entry.entry_id)},
+            "name": "RBFA",  # Préfixe qui apparaîtra pour toutes les entités
+            "manufacturer": "RBFA",
+            "model": "Football Matches",
+        }
